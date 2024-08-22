940abb9ca4d6b304490f503efdf6dd51b4e47250
Aiman Qureshi <aiman@pcbase.co.uk>
2024-08-22 12:26:12 +0100

    Version14

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // JavaScript source code
 console.log('config.js');
-console.log('version13');
+console.log('version14');
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 // JavaScript source code
 console.log('index.js');
-console.log('version13');
+console.log('version14');
